3a40d920fede17f7d6c1ee9519cedc771c5060b2
Artem Bilodid <ar.bilodid@draftkings.com>
2021-09-20 18:53:23 +0300

    initial commit; first task ready

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,31 @@
+import styles from "./profile.module.scss";
+
+const Profile = (props) => {
+  const { name, tag, location, avatar, stats } = props;
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="Аватар пользователя" className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li className={styles.stats__item}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li className={styles.stats__item}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
+        </li>
+        <li className={styles.stats__item}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Profile;
